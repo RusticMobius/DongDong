23443d211b703f2d43a50e8daac3b2ebde1c7793
unknown <191250100@smail.nju.edu.cn>
2022-10-26 22:23:36 +0800

    feature:基本框架搭建

---
+++ app/src/main/java/com/example/dongdongapp/util/dongHTTPClient.java
@@ -0,0 +1,18 @@
+package com.example.dongdongapp.util;
+
+import okhttp3.OkHttpClient;
+
+/*
+TODO:封装HTTP请求过程
+ */
+public class dongHTTPClient {
+
+    private OkHttpClient okHttpClient=new OkHttpClient();
+
+    //TODO:GET请求
+    public String doGet(String url){
+        return  null;
+    }
+
+    
+}
