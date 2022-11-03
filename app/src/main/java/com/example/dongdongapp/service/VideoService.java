49624b7a167a3481314a86a82413aa283211ad0f
陆健成 <191250100@smail.nju.edu.cn>
2022-11-03 21:15:26 +0800

    feat:working on #5442#，初步实现视频上传到后端的逻辑

---
+++ app/src/main/java/com/example/dongdongapp/service/VideoService.java
@@ -0,0 +1,33 @@
+package com.example.dongdongapp.service;
+
+import com.example.dongdongapp.config.dongdongappConfiguration;
+import com.example.dongdongapp.util.DongHTTPClient;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
+public class VideoService {
+
+    private final String backendUrl= dongdongappConfiguration.backendUrl+"/video";
+
+    /**
+     * 上传拍摄的运动视频
+     * @param path 视频文件位置
+     * @param type 运动类型
+     */
+    public void uploadVideo(String path,String type){
+        File file=new File(path);
+        RequestBody requestBody=RequestBody.create(MediaType.parse("video/*"),file);
+        MultipartBody formBody=new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", file.getName(),requestBody)
+                .addFormDataPart("type",type)
+                .build();
+
+        DongHTTPClient dongHTTPClient=new DongHTTPClient();
+        String result=dongHTTPClient.doPost(backendUrl,formBody);
+    }
+}
--- app/src/main/java/com/example/dongdongapp/util/DongHTTPClient.java
+++ app/src/main/java/com/example/dongdongapp/util/DongHTTPClient.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -101,5 +102,44 @@ public class DongHTTPClient {
         return  this.result;
     }
 
-    
+    /**
+     * POST请求（上传视频用）
+     * @param url 目标url
+     * @param form 需要POST的表单
+     * @return
+     */
+    public String doPost(String url, MultipartBody form){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient=new OkHttpClient();
+                Request request=new Request.Builder()
+                        .url(url)
+                        .post(form)
+                        .build();
+
+                Call call=okHttpClient.newCall(request);
+
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.d("dongHTTPClient","msg"+e);
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        result=response.body().string();
+                    }
+                });
+            }
+        }).start();
+        while (this.result.length()==0){
+            try{
+                TimeUnit.MILLISECONDS.sleep(10);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        return  this.result;
+    }
 }
